9c3e936e98d64a72afd32168f59462cc0762e1ac
Quincy Hicks <qdh6159@gmail.com>
2019-11-25 10:30:02 -0600

    add profile and image

--- src/components/MainBox.jsx
+++ src/components/MainBox.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import CardTable from './CardTable'
-
+import CardTable2 from './Profile'
 
 function MainBox() {
     return (
       <div class="mainBox">
         {/* <h1> Main Box</h1> */}
         <CardTable />
+        <CardTable2 />
       </div>
     );
   }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function CardTable2() {
+    return (
+      <div class="cardTable2">
+          <img class="profilePic" src="" alt="Paris" width="300" height="300"></img>
+
+      </div>
+    );
+  }
+  
+  export default CardTable2;

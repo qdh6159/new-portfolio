909bb93dfd88c0c8b181618ebe7aff306331412d
Quincy Hicks <qdh6159@gmail.com>
2019-11-25 08:22:09 -0600

    added image

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,7 +9,7 @@ const Cards = (props) => {
       <ul>
           <li>
           <Card >
-        <CardImg width="100%" src="https://cdn.shopify.com/s/files/1/1721/2115/products/RickandMorty_1024x1024.jpg?v=1539628583" alt="Card image cap" />
+        <CardImg width="50%" src="https://cdn.shopify.com/s/files/1/1721/2115/products/RickandMorty_1024x1024.jpg?v=1539628583" alt="Card image cap" />
         <CardImgOverlay>
           <p id="titles">Rick's World API</p>
           
@@ -21,7 +21,7 @@ const Cards = (props) => {
 
           <li>
           <Card >
-        <CardImg width="100%" src="my-portfolio-website/images/UNADJUSTEDNONRAW_thumb_23e8.jpg" alt="Card image cap" />
+        <CardImg width="100%" src="https://raw.githubusercontent.com/qdh6159/qdh6159.github.io/master/images/UNADJUSTEDNONRAW_thumb_23e8.jpg" alt="Card image cap" />
         <CardImgOverlay>
         <p id="titles">Resume</p>
           

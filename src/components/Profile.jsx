89b99d5b496e2f738dd9f98a21b7d9a97adb0975
Quincy Hicks <qdh6159@gmail.com>
2019-11-25 10:54:15 -0600

    updated profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,10 +4,17 @@ import React from 'react'
 function CardTable2() {
     return (
       <div class="cardTable2">
-          <img class="profilePic" src="" alt="Paris" width="300" height="300"></img>
+        <img class="profilePic" src="https://raw.githubusercontent.com/qdh6159/new-portfolio/master/images/SphWTuiPR9C5NBvskNjSaQ_thumb_23ea.jpg" alt="Paris" width="300" height="300"></img>
+        <p>qdh6159@gmail.com</p>
+        <p>+1 (318)-272-4790</p>
 
+        <h4>About</h4>
+
+        <p>Welcome to my portfolio site! I’m coding to change the world in a positive way. I love pizza, Spotify, honest answers and my dog, Yoda. I like stretching to keep me grounded in life. I jam on my Fender Strat in my spare time. </p>
       </div>
     );
   }
   
   export default CardTable2;
+
+

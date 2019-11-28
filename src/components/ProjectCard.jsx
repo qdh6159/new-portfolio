12f5f2639c17f3a75349f1d7190b9271a218708a
Quincy Hicks <qdh6159@gmail.com>
2019-11-27 19:08:08 -0600

    updated cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -25,8 +25,28 @@ export default function ProjectCard() {
                 <CardBody>
                 <CardTitle>Lorem Ipsum</CardTitle>
                 <p>Lorem ipsum dolor sit amet.</p>
-                <Button>Read more &rarr;</Button>
                 </CardBody>
+                <CardFooter style={{ textAlign: "left" }}>
+                    <Button outline size="sm" style={{ marginRight: "10px" }}>Read more &rarr;</Button>
+                    <Button outline size="sm" theme="secondary">Secondary</Button>
+                </CardFooter>
+            </Card>
+            </Col> 
+
+            <Col>
+            <Card style={{ maxWidth: "700px" }, {width: "350px"}}>
+                <CardImg style={{ maxHeight: "250px" }} src="https://images-na.ssl-images-amazon.com/images/I/61IL9YgWyrL.jpg" />
+                <CardBody>
+                <CardTitle>Lorem Ipsum</CardTitle>
+                <p>Lorem ipsum dolor sit amet.</p>
+                
+                
+                </CardBody>
+
+                <CardFooter style={{ textAlign: "left" }}>
+                    <Button outline size="sm" style={{ marginRight: "10px" }}>Read more &rarr;</Button>
+                    <Button outline size="sm" theme="secondary">Secondary</Button>
+                </CardFooter>
             </Card>
             </Col> 
 
@@ -36,19 +56,13 @@ export default function ProjectCard() {
                 <CardBody>
                 <CardTitle>Lorem Ipsum</CardTitle>
                 <p>Lorem ipsum dolor sit amet.</p>
-                <Button>Read more &rarr;</Button>
+                
                 </CardBody>
-            </Card>
-            </Col> 
-
-            <Col>
-            <Card style={{ maxWidth: "700px" }, {width: "350px"}}>
-                <CardImg style={{ maxHeight: "250px" }} src="https://raw.githubusercontent.com/qdh6159/new-portfolio/master/images/Image%2011-25-19%20at%208.19%20AM.jpg" />
-                <CardBody>
-                <CardTitle>Lorem Ipsum</CardTitle>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <Button>Read more &rarr;</Button>
-                </CardBody>
+                <CardFooter style={{ textAlign: "left" }}>
+                    <Button outline size="sm" style={{ marginRight: "10px" }}>Read more &rarr;</Button>
+                    <Button outline size="sm" theme="secondary">Secondary</Button>
+                </CardFooter>
+                
             </Card>
             </Col> 
             </Row>
